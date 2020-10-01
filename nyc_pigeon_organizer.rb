9ce89ecda289a793f1892f7d952b4c76632b3ec9
Roberto Acosta <x.rojo@hotmail.com>
2020-10-01 04:46:30 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,10 +1,10 @@
 def nyc_pigeon_organizer(data)
   names = {}
   data.each do |attributes, categ|
-    categ.each do |specs, all_names|
-      all_names.each do |name|
-        if pigeon_list[name] == nil
-          pigeon_list[name] = {}
+    categ.each do |specs, indiv|
+      indiv.each do |identity|
+        if names[identity] == nil
+          names[identity] = {}
         end
         if pigeon_list[name][color_gender_lives] == nil
           pigeon_list[name][color_gender_lives] = []
